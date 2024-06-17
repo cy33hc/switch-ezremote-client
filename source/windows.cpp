fa2ea7b6a74a5623b7e3fcce413221d2cbc2a203
Chee Yee <cheeyee09@gmail.com>
2024-06-16 23:56:36 -0700

    implement wrap around while scrolling file list

--- source/windows.cpp
+++ source/windows.cpp
@@ -25,6 +25,9 @@ static u64 pad_prev;
 bool paused = false;
 int view_mode;
 static float scroll_direction = 0.0f;
+static int selected_local_position = -1;
+static int selected_remote_position = -1;
+
 static ime_callback_t ime_callback = nullptr;
 static ime_callback_t ime_after_update = nullptr;
 static ime_callback_t ime_before_update = nullptr;
@@ -540,6 +543,22 @@ namespace Windows
                     ImGui::Text(item.name);
                     ImGui::EndTooltip();
                 }
+                if (ImGui::IsKeyPressed(ImGuiKey_GamepadDpadUp) && !paused)
+                {
+                    if (j == 0)
+                    {
+                        selected_local_position = local_files.size()-1;
+                        scroll_direction = 0.0f;
+                    }
+                }
+                else if (ImGui::IsKeyPressed(ImGuiKey_GamepadDpadDown) && !paused)
+                {
+                    if (j == local_files.size()-1)
+                    {
+                        selected_local_position = 0;
+                        scroll_direction = 1.0f;
+                    }
+                }
             }
             if (ImGui::IsWindowFocused(ImGuiFocusedFlags_ChildWindows))
             {
@@ -548,6 +567,12 @@ namespace Windows
                     SetNavFocusHere();
                     ImGui::SetScrollHereY(0.5f);
                     sprintf(local_file_to_select, "");
+                }
+                if (selected_local_position == j && !paused)
+                {
+                    SetNavFocusHere();
+                    ImGui::SetScrollHereY(scroll_direction);
+                    selected_local_position = -1;
                 }
                 selected_browser |= LOCAL_BROWSER;
             }
@@ -700,6 +725,22 @@ namespace Windows
                     ImGui::Text(item.name);
                     ImGui::EndTooltip();
                 }
+                if (ImGui::IsKeyPressed(ImGuiKey_GamepadDpadUp) && !paused)
+                {
+                    if (j == 0)
+                    {
+                        selected_remote_position = remote_files.size()-1;
+                        scroll_direction = 0.0f;
+                    }
+                }
+                else if (ImGui::IsKeyPressed(ImGuiKey_GamepadDpadDown) && !paused)
+                {
+                    if (j == remote_files.size()-1)
+                    {
+                        selected_remote_position = 0;
+                        scroll_direction = 1.0f;
+                    }
+                }
             }
             ImGui::PopID();
             if (ImGui::IsItemFocused())
@@ -713,6 +754,12 @@ namespace Windows
                     SetNavFocusHere();
                     ImGui::SetScrollHereY(0.5f);
                     sprintf(remote_file_to_select, "");
+                }
+                if (selected_remote_position == j && !paused)
+                {
+                    SetNavFocusHere();
+                    ImGui::SetScrollHereY(scroll_direction);
+                    selected_remote_position = -1;
                 }
                 selected_browser |= REMOTE_BROWSER;
             }
